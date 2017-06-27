cd856e594b57cd5b85807203c4abda81e06cb820
Omar Lari <oalari@gmail.com>
2017-06-27 10:36:18 -0700

    Omar Lari- 27 June attempt <oalari@gmail.com>

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -1,12 +1,19 @@
 package guru99.gittest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import com.google.common.*;
 
 public class AppTest {
 
 	@Test
 public void testJ(){
+		WebDriver driver;
 	System.out.println("Hello world!!!");
+	System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.get("http://www.our-africa.org/ethiopia/");
 }
  
 }
